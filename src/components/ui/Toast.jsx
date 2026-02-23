d6d9538bde526112f77ab6ad58744d756ed25dac
Ibrahim Bello <ibrahimopeyemibello@gmail.com>
2026-02-23 13:37:17 +0100

    fixed toast bug

--- src/components/ui/Toast.jsx
+++ src/components/ui/Toast.jsx
@@ -15,13 +15,14 @@ function Toast({ toast, onDone, duration = 3000 }) {
       ? "bg-green-500"
       : toast.type === "warning"
         ? "bg-yellow-500"
-        : "bg-gray-800";
+        : "bg-red-800";
 
   return (
     <div className="fixed top-4 left-1/2 -translate-x-1/2 z-50 w-80 rounded-xl shadow-2xl overflow-hidden bg-pale-primary">
       <div
         className={`px-4 py-3 text-sm font-medium flex items-center gap-2 text-primary `}
       >
+        <span className={`${bg} size-3 rounded-full inline-block`}></span>
         {toast.message}
       </div>
 
